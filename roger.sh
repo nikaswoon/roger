0e55a10172cee9e6d8b848aa82e714471635bfa2
tasmity <tatyana@tasmity.ru>
2020-01-24 12:19:19 +0300

    add roger

--- roger.sh
+++ roger.sh
@@ -1,3 +1,4 @@
+
 1. Создане пользователя во время установки - tasmity
 
 2. Разбивка диска во время установки
@@ -10,22 +11,25 @@
 
 4. Добавление пользователя в группу sudo 
 	usermod -a -G sudo tasmity
+	usermod -a -G ssh tasmity
 
 5. Редактируем файл /etc/network/interfaces
 	auto enp0s8
 	iface enp0s8 inet static
-	address 192.168.0.33
+	address 192.168.100.2
 	netmask 255.255.255.252
 
 6 Создание ключей ssh
 	ssh-keygen -t rsa
-	ssh-copy-id -i ~/.ssh/id_rsa.pub tasmity@192.168.0.33
+	ssh-copy-id -i ~/.ssh/id_rsa.pub tasmity@192.168.100.2
 
 7. Настрой ssh, редактирование файла
 	/etc/ssh/sshd_config
 	- смена порта
 	- запрет аунтификации по паролю
 	- отграничением доступа по группе
+		AllowGroups ssh
+		DenyGroups root
 	sudo systemctl restart sshd
 
 8. Обновление пакетов
@@ -38,25 +42,147 @@
 
 10. Настройа брандмауэра
 	sudo apt-get install ufw
-	- смена порта ssh /etc/ufw/applications.d/openssh-server
+	- смена порта sudo nano ssh /etc/ufw/applications.d/openssh-server
 	sudo ufw default deny incoming
 	sudo ufw default allow outgoing
 	sudo ufw allow OpenSSH
-	sudo ufw allow 443
-	sudo ufw allow 80/tcp
+	sudo ufw allow 'WWW Full'
+	sudo ufw enable
 	sudo ufw status
+	
+	sudo apt-get install iptables-persistent
+	Источник: https://liberatum.ru/blog/iptables-linux-secure
+	# Запрет FIN-сканирования
+	sudo iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,ACK FIN -j DROP
+	# Запрет X-сканирования
+	sudo iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG FIN,SYN,RST,PSH,ACK,URG -j DROP
+	# Запрет N-сканирования
+	sudo iptables -A INPUT –p tcp –m tcp –-tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE –j DROP
+	# Запрет SYN/ACK-сканирования
+	sudo iptables -I INPUT -p tcp -m osf --genre NMAP -j DROP
+	# Проверка на стук в нерабочие порты (10 в час)
+	sudo iptables -A INPUT -m recent --rcheck --seconds 3600 --hitcount 10 --rttl -j RETURN
+	# Вторая проверка на стук в нерабочие порты (2 в минуту)
+	sudo iptables -A INPUT -m recent --rcheck --seconds 60 --hitcount 2 --rttl -j RETURN
+	# Заносим адреса стучащихся в список
+	sudo iptables -A INPUT -m recent --set
+	# Отбрасываем пакеты всех, кто превысил лимит на количество подключений
+	sudo iptables -P INPUT DROP
+	sudo iptables-save > /etc/iptables/rules.v4
+	sudo systemctl restart ufw
+
 	
 11. Защита от DOS атак на открытых портах:
 	sudo apt-get install fail2ban
-	- отредактировать файл /etc/fail2ban/jail.conf
-	[sshd]
-	enabled = true
-	port    = 7412
-	logpath = %(sshd_log)s
-	backend = %(sshd_backend)s
-	maxretry = 3
-	bantime = 600
-	Включить фильтры apache
+	- /etc/fail2ban/jail.d/defaults-debian.conf
+	[DEFAULT]
+bantime   = 600
+findtime  = 10m
+maxretry  = 2
+
+
+# SSH jail
+[sshd]
+enabled   = true
+port    = 7412
+logpath = %(sshd_log)s
+backend = %(sshd_backend)s
+maxretry  = 3
+bantime   = 600
+findtime  = 10m
+
+# ddos SSH jail
+enabled = true
+port = 7412
+filter = sshd-ddos
+logpath = %(sshd_log)s
+maxretry = 2
+
+[ssh-iptables-ipset4]
+enabled = true
+port = 7412
+filter = sshd
+action = iptables[name=SSH, port=ssh, protocol=tcp]
+logpath = %(sshd_log)s
+findtime = 300
+maxretry = 3
+bantime = 600
+
+[dropbear]
+enabled = true
+port = 7412
+logpath  = %(dropbear_log)s
+backend  = %(dropbear_backend)s
+
+# APACHE JAILS
+[apache]
+enabled = true
+port = http,https
+filter = apache-auth
+logpath  = %(apache_error_log)s
+maxretry = 3
+
+[apache-multiport]
+enabled = true
+port = http,https
+filter = apache-auth
+logpath  = %(apache_error_log)s
+maxretry = 3
+
+[apache-auth]
+enabled  = true
+port     = http,https
+maxretry = 3
+bantime  = 10800
+findtime = 3600
+
+[apache-noscript]
+enabled = true
+port     = http,https
+logpath  = %(apache_error_log)s
+filter = apache-noscript
+maxretry = 3
+
+[apache-overflows]
+enabled = true
+port     = http,https
+logpath  = %(apache_error_log)s
+maxretry = 2
+
+
+[postfix]
+enabled = true
+mode    = more
+port    = smtp,465,submission
+logpath = %(postfix_log)s
+backend = %(postfix_backend)s
+action = iptables[name=Postfix-smtp, port=smtp, protocol=tcp]
+filter = postfix
+bantime = 86400
+maxretry = 3
+findtime = 3600
+ignoreip = 127.0.0.1
+
+(разблокировка sudo fail2ban-client set ssh-iptables unbanip 61.147.103.113)
+
+
+	- sudo apt-get install libapache2-mod-evasive
+	Создаем файл настроек mod-evasive  /etc/apache2/mods-available/mod-evasive.conf
+	<IfModule mod_evasive20.c>
+	DOSHashTableSize 4096
+	DOSPageCount 30
+	DOSSiteCount 150
+	DOSPageInterval 1
+	DOSSiteInterval 1
+	DOSBlockingPeriod 30
+	DOSWhiteList 192.168.0.*
+	DOSWhiteList 10.211.55.*
+	DOSLogDir "/var/log/mod_evasive"
+	DOSEmailNotify root@localhost
+	</IfModule>
+	-sudo a2enmod mod-evasive
+	sudo service apache2 restart
+	(скрипт для теста /usr/share/doc/libapache2-mod-evasive/examples/test.pl)
 	
 12. Защита от сканирования открытых портов:
 	sudo apt-get install portsentry
@@ -70,6 +196,9 @@
 	KILL_HOSTS_DENY="ALL: $TARGET$ : DENY"
 	KILL_RUN_CMD=""/sbin/iptables -I INPUT -s $TARGET$ -j DROP && /sbin/iptables -I INPUT -s $TARGET$ -m limit --limit 3/minute --limit-burst 5 -j LOG --log-level debug --log-prefix 'Portsentry: dropping: '"
 	PORT_BANNER="** UNAUTHORIZED ACCESS PROHIBITED *** YOUR CONNECTION ATTEMPT HAS BEEN LOGGED. GO AWAY."/"
+	- проверка
+	nmap 192.168.100.2
+	/ets/hosts.deny
 	
 13. Остановка ненужных услуг:
 	- список услуг со статусом
@@ -79,7 +208,7 @@
 	sudo systemctl disable keyboard-setup.service
 	sudo systemctl disable apt-daily.timer
 	sudo systemctl disable apt-daily-upgrade.timer
-	sudo systemctl disable syslog.service
+	#sudo systemctl disable syslog.service
 	
 14. Создание скрипта который обновляет исходные файлы пакетов, а затем и сами пакеты
 	- скрипт update.sh
@@ -97,6 +226,7 @@
 	- установка почты:
 	sudo apt-get install mailutils
 	sudo apt-get install postfix
+	sudo dpkg-reconfigure postfix 
 	- настройка почты
 	редактировть /etc/aliases
 	root: root
@@ -111,6 +241,7 @@
 	fi
 	- права на выполнение
 	sudo chmod +x newcron.sh
+	sudo md5sum /etc/crontab > /var/lib/cron.md5
 	- sudo crontab -e
 	0 0 * * * root /etc/cron.d/scripts/newcron.sh
 	
@@ -145,12 +276,12 @@
 	- настройка хоста
 	sudo nano /etc/apache2/sites-available/default-ssl.conf
 	ServerAdmin tasmity@localhost
-	ServerName 192.168.0.33
+	ServerName 192.168.100.2
 	SSLCertificateFile      /etc/ssl/certs/apache-selfsigned.crt
 	SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
 	- настройка переадресации
 	sudo nano /etc/apache2/sites-available/000-default.conf
-	Redirect "/" "https://192.168.0.33/"
+	Redirect "/" "https://192.168.100.2/"
 	- включите модуль Apache для SSL
 	sudo a2enmod ssl
 	sudo a2enmod headers
@@ -162,6 +293,10 @@
 	sudo apache2ctl configtest
 	- перезапуск сервера
 	sudo systemctl restart apache2
+	Redirect permanent "/" "https://192.168.100.2/"
+	sudo apache2ctl configtest
+	sudo systemctl restart apache2
+	
 19. Автоматизация развертывания сайта
 	- установки git
 	sudo apt-get install git
@@ -173,7 +308,7 @@
 	git init --bare
 	- создание хука#
 	nano hooks/post-receive
-	!/bin/bash
+	#!/bin/bash
 	while read oldrev newrev ref
 	do
 	if [[ $ref =~ .*/master$ ]];
@@ -189,8 +324,4 @@
 	- локальные настройки
 	git clone ssh://roger:/home/tasmity/site.git repo
 	git remote add production ssh://roger:/home/tasmity/site.git
-	
-	
-	
-	
 	
